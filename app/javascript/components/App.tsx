a35331ee9ad00c11b98fc9cdacdb1bf1aadfbad3
Hamir Mahal <hamirmahal@gmail.com>
2023-04-03 21:21:36 -0700

    refactor: remove redundant code

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -1,23 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
-import Filer from '../types';
 import './App.css';
 import FilerPage from './FilerPage';
 import Homepage from './Homepage';
 
 function App() {
-  const [filers, setFilers] = useState<Array<Filer>>();
-
-  useEffect(() => {
-    fetch(`/api/v1/filers`)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setFilers(data);
-      });
-  }, []);
-
   return (
     <div className="App">
       <Router>
