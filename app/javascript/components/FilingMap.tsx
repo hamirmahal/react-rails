679100e20ca7ad33fa2503915904f6b59ac43dcd
Hamir Mahal <hamirmahal@gmail.com>
2023-04-07 12:15:24 -0700

    style: label recipients by state section with `h3`

--- app/javascript/components/FilingMap.tsx
+++ app/javascript/components/FilingMap.tsx
@@ -60,61 +60,74 @@ const FilingMap: React.FC<Props> = ({ filer }) => {
   }, []);
 
   return stateToNumRecipients?.size ? (
-    <ComposableMap
-      projection="geoAlbersUsa"
-      projectionConfig={{
-        scale: 1000
-      }}
-    >
-      <Geographies geography={geoUrl}>
-        {({ geographies }) => (
-          <>
-            {geographies.map((geo) => (
-              <Geography
-                key={geo.rsmKey}
-                stroke="#FFF"
-                geography={geo}
-                fill="#DDD"
-              />
-            ))}
-            {geographies.map((geo) => {
-              const centroid = geoCentroid(geo);
-              const cur = allStates.find((s) => s.val === geo.id);
-              return (
-                <g key={geo.rsmKey + '-name'}>
-                  {cur &&
-                    centroid[0] > -160 &&
-                    centroid[0] < -67 &&
-                    (Object.keys(offsets).indexOf(cur.id) === -1 ? (
-                      <Marker coordinates={centroid}>
-                        <text y="2" fontSize={10} textAnchor="middle">
-                          {cur.id}{' '}
-                          {loading
-                            ? ''
-                            : stateToNumRecipients?.get(cur.id) ?? 0}
-                        </text>
-                      </Marker>
-                    ) : (
-                      <Annotation
-                        subject={centroid}
-                        dx={offsets[cur.id][0]}
-                        dy={offsets[cur.id][1]}
-                      >
-                        <text x={4} fontSize={14} alignmentBaseline="middle">
-                          {cur.id}{' '}
-                          {loading
-                            ? ''
-                            : stateToNumRecipients?.get(cur.id) ?? 0}
-                        </text>
-                      </Annotation>
-                    ))}
-                </g>
-              );
-            })}
-          </>
-        )}
-      </Geographies>
-    </ComposableMap>
+    <>
+      <h3
+        style={{
+          // The `ComposableMap` component's top padding already
+          // results in a significant vertical gap between itself and
+          // the element before it.
+          marginBottom: -40,
+          marginTop: 100
+        }}
+      >
+        Recipients by state
+      </h3>
+      <ComposableMap
+        projection="geoAlbersUsa"
+        projectionConfig={{
+          scale: 1000
+        }}
+      >
+        <Geographies geography={geoUrl}>
+          {({ geographies }) => (
+            <>
+              {geographies.map((geo) => (
+                <Geography
+                  key={geo.rsmKey}
+                  stroke="#FFF"
+                  geography={geo}
+                  fill="#DDD"
+                />
+              ))}
+              {geographies.map((geo) => {
+                const centroid = geoCentroid(geo);
+                const cur = allStates.find((s) => s.val === geo.id);
+                return (
+                  <g key={geo.rsmKey + '-name'}>
+                    {cur &&
+                      centroid[0] > -160 &&
+                      centroid[0] < -67 &&
+                      (Object.keys(offsets).indexOf(cur.id) === -1 ? (
+                        <Marker coordinates={centroid}>
+                          <text y="2" fontSize={10} textAnchor="middle">
+                            {cur.id}{' '}
+                            {loading
+                              ? ''
+                              : stateToNumRecipients?.get(cur.id) ?? 0}
+                          </text>
+                        </Marker>
+                      ) : (
+                        <Annotation
+                          subject={centroid}
+                          dx={offsets[cur.id][0]}
+                          dy={offsets[cur.id][1]}
+                        >
+                          <text x={4} fontSize={14} alignmentBaseline="middle">
+                            {cur.id}{' '}
+                            {loading
+                              ? ''
+                              : stateToNumRecipients?.get(cur.id) ?? 0}
+                          </text>
+                        </Annotation>
+                      ))}
+                  </g>
+                );
+              })}
+            </>
+          )}
+        </Geographies>
+      </ComposableMap>
+    </>
   ) : (
     <></>
   );
